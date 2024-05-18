42c5333c3024f80781873026b6e62dd29131aa71
root <root@NTL7D>
2024-05-18 23:10:04 +0700

    change api response back to class

--- server/src/main/java/com/ntl7d/server/dto/ApiResponse.java
+++ server/src/main/java/com/ntl7d/server/dto/ApiResponse.java
@@ -2,6 +2,15 @@ package com.ntl7d.server.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public record ApiResponse<T>(boolean success, String message, T result) {
+public class ApiResponse<T> {
+    boolean success;
+    String message;
+    T result;
 }
